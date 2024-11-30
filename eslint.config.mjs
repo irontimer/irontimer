0a05c70a192d16c8417fdc95d6dc1bb2778b1d45
Evan <evanrayweigel@outlook.com>
2024-11-30 12:18:46 -0600

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,89 +1,96 @@
-import prettier from "eslint-plugin-prettier";
-import solid from "eslint-plugin-solid";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierConfig from "eslint-config-prettier";
+import prettier from "eslint-plugin-prettier";
+import solid from "eslint-plugin-solid/configs/recommended";
+import typescriptEslintPlugin from "eslint-plugin-solid/configs/typescript";
+import globals from "globals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+export default [
+	js.configs.recommended,
+	prettierConfig,
+	typescriptEslintPlugin,
+	{
+		plugins: {
+			prettier,
+			"@typescript-eslint": typescriptEslint,
+		},
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.browser,
+			},
 
-export default [...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:solid/recommended",
-    "prettier",
-), {
-    plugins: {
-        prettier,
-        solid,
-        "@typescript-eslint": typescriptEslint,
-    },
+			parser: tsParser,
+			ecmaVersion: "latest",
+			sourceType: "module",
 
-    languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.browser,
-        },
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
+				project: "**/tsconfig.json",
+			},
+		},
+		rules: {
+			"prettier/prettier": "error",
+			"linebreak-style": ["error", "unix"],
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
+			indent: [
+				"error",
+				"tab",
+				{
+					SwitchCase: 1,
+				},
+			],
+		},
+	},
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		...solid,
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				project: "tsconfig.json",
+			},
+		},
+		rules: {
+			"no-unused-vars": "off",
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{
+					argsIgnorePattern: "^_",
+				},
+			],
 
-            project: "**/tsconfig.json",
-        },
-    },
+			"@typescript-eslint/explicit-function-return-type": [
+				"error",
+				{
+					allowExpressions: true,
+					allowTypedFunctionExpressions: true,
+					allowHigherOrderFunctions: true,
+				},
+			],
 
-    rules: {
-        "prettier/prettier": "error",
-        "linebreak-style": ["error", "unix"],
+			"@typescript-eslint/strict-boolean-expressions": [
+				"error",
+				{
+					allowString: false,
+					allowNumber: false,
+					allowNullableObject: false,
+				},
+			],
 
-        indent: ["error", "tab", {
-            SwitchCase: 1,
-        }],
-    },
-}, {
-    files: ["**/*.ts", "**/*.tsx"],
-
-    rules: {
-        "no-unused-vars": "off",
-
-        "@typescript-eslint/no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-        }],
-
-        "@typescript-eslint/explicit-function-return-type": ["error", {
-            allowExpressions: true,
-            allowTypedFunctionExpressions: true,
-            allowHigherOrderFunctions: true,
-        }],
-
-        "@typescript-eslint/strict-boolean-expressions": ["error", {
-            allowString: false,
-            allowNumber: false,
-            allowNullableObject: false,
-        }],
-
-        curly: ["error", "all"],
-        quotes: ["error", "double"],
-        "arrow-parens": ["error", "always"],
-        semi: ["error", "always"],
-        "comma-dangle": ["error", "always-multiline"],
-        "solid/no-array-handlers": "error",
-        "solid/prefer-classlist": "error",
-        "solid/prefer-show": "error",
-    },
-}];
+			curly: ["error", "all"],
+			quotes: ["error", "double"],
+			"arrow-parens": ["error", "always"],
+			semi: ["error", "always"],
+			"comma-dangle": ["error", "always-multiline"],
+			"solid/no-array-handlers": "error",
+			"solid/prefer-classlist": "error",
+			"solid/prefer-show": "error",
+		},
+	},
+];
