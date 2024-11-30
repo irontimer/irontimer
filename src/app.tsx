5183ffaad6606bb576458f73df15b33724c5a0b3
Evan <evanrayweigel@outlook.com>
2024-11-30 11:56:29 -0600

    spaces -> tabs

--- src/app.tsx
+++ src/app.tsx
@@ -4,17 +4,17 @@ import { Suspense } from "solid-js";
 import "./app.scss";
 
 export default function App() {
-  return (
-    <Router
-      root={props => (
-        <>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+	return (
+		<Router
+			root={(props) => (
+				<>
+					<a href="/">Index</a>
+					<a href="/about">About</a>
+					<Suspense>{props.children}</Suspense>
+				</>
+			)}
+		>
+			<FileRoutes />
+		</Router>
+	);
 }
